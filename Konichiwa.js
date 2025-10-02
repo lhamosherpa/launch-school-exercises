dc7736a064b261827b0f94fc40d083f44b124c47
Lhamo Sherpa Bhote <lhamo.sherpabhote@gmail.com>
2025-10-01 20:05:09 -0400

    My first git commit

--- Konichiwa.js
+++ Konichiwa.js
@@ -1,1 +1,32 @@
+let age = 20
+console.log(`you are ${age} years old`)
+console.log(`In 10 years, you will be ${age + 10} years old`)
+console.log(`In 20 years, you will be ${age + 20} years old`)
+console.log(`In 30 years, you will be ${age + 30} years old`)
+let girl = 'Lhamo'
+console.log(`Good Morning,${girl}`);
+console.log(`Good Afternoon,${girl}`);
+console.log(`Good Evening,${girl}`);
+let firstNameA = ('Lhamo')
+console.log(firstNameA)
+let firstName
+console.log(firstName)
+let answer = 41
+answer = 42
+console.log(answer)
+console.log(12<9)
+console.log('12'<'9')
+console.log(parseInt('3.1415'))
+console.log('foo'==='Foo')
+let pets = {
+    asta: 'dog',
+    butterscotch: 'cat',
+    pudding: 'dog',
+    neptune: 'fish',
+    darwin: 'lizard',
+}
+console.log(pets)
+let names = ['asta','butterscotch', 'pudding','neptune','darwin']
+console.log(names)
+console.log(5 + 10)
 console.log('it\'s a greeting in Japanese Language')
--- Tashi Delek.js
+++ Tashi Delek.js
@@ -1,1 +1,12 @@
+let person = 'Victor';
+console.log ('person')
+console.log ('good morning, ${person}.')
+console.log ('good afternoon, ${person}.')
+console.log ('good evening, ${person}.')
+
+
+
+
+{let foo = 42
+console.log(foo)}
 console.log('it\'s a greeting in Tibetan language')

0bc74819fc28352cb9ed62b1c87ea48c49f17bcb
Lhamo Sherpa Bhote <lhamo.sherpabhote@gmail.com>
2025-10-03 19:23:42 -0400

    I am trying to do my first try commit

---
+++ Lhamo/chipu.js
@@ -0,0 +1,1 @@
+console.log('Lhamo sister call this by nickname')
---
+++ Practices/prepatory.js
@@ -0,0 +1,5 @@
+let L = 'Lhamo'
+let B = 'Iuliu'
+let C = 'Love'
+console.log(`${L + C + B}`)
+console.log('This is the first file for just prepatory')

b9a4516de3be4e3c0714db61b88a067a3cab978d
Lhamo Sherpa Bhote <lhamo.sherpabhote@gmail.com>
2025-12-06 17:30:38 -0500

    practice

---
+++ Practices/prepatoryend.js
@@ -0,0 +1,1 @@
+console.log('This is the file for end section of the prepatory')
---
+++ Wednesday/Afternoon.js
@@ -0,0 +1,12 @@
+console.log('This is for the afternoon log')
+console.log(2+2)
+console.log('Tashi'+' ' +'Lhamo')
+let a = 12 
+console.log(`I am ${a} years old`)
+console.log(`Next year, I will be ${a + 1} years old`)
+console.log(`In 5 years, I will be ${a + 5} years old`)
+let b = 4 
+console.log (a +b)
+console.log(`This is a trial log`)
+console.log (`Tashi`+` ` + `Sonam`)
+console.log({fruits:'apple', animal:'cat', object:'table'}['fruits'])
